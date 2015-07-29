ce2e8a43cc9d1595428249dad43bc6d179bbaa7a
Mohammad Al-Turany <m.al-turany@gsi.de>
2015-07-29 13:29:09 +0200

    update docs

---
+++ search/searchdata.js
@@ -0,0 +1,48 @@
+var indexSectionsWithContent =
+{
+  0: "_abcdefghiklmnopqrstuvwxyz~",
+  1: "abcdefghilmoprstuvwx",
+  2: "a",
+  3: "abcdefghilmoprstuvwx",
+  4: "abcdefghilmnopqrstuvwxyz~",
+  5: "abcdefghikmnoprstvxyz",
+  6: "acdhmp",
+  7: "abcdepsu",
+  8: "abcdehiklmnopst",
+  9: "bfglmo",
+  10: "_ahks",
+  11: "c"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes",
+  2: "namespaces",
+  3: "files",
+  4: "functions",
+  5: "variables",
+  6: "typedefs",
+  7: "enums",
+  8: "enumvalues",
+  9: "related",
+  10: "defines",
+  11: "groups"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes",
+  2: "Namespaces",
+  3: "Files",
+  4: "Functions",
+  5: "Variables",
+  6: "Typedefs",
+  7: "Enumerations",
+  8: "Enumerator",
+  9: "Friends",
+  10: "Macros",
+  11: "Modules"
+};
+
